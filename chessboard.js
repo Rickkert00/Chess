eced61fff40046ca4ba340a5b5c105dd6f5a79dd
Jahson <jahson.binda@gmail.com>
2018-11-24 15:11:39 +0100

    Added Chess Piece Ascii codes+CSS and unique tile ID

--- chessboard.js
+++ chessboard.js
@@ -1,11 +1,11 @@
 //the creation of the chessboard tiles with each tile having its own unique id
 
 $(document).ready(function() {
-    var space = 1;
-    for (var r = 0; r<8; r++) {
+    for (var r = 8; r>0; r--) {
         var col = "";
-        for (var c = 0; c<8; c++) {
-            col+="<td pos='"+space+"'></td>"; space++; }
+        for (var c = 65; c<73; c++) {
+            var cString = String.fromCharCode(c);
+            col+="<td id='"+ cString + String.toString(r) +"'></td>"; }
             $("#board").append("<tr>"+col+"</tr>");
         }
     }
